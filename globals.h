f1184d5a2337882b53b7f470bace84974104dcaa
doug@luna <doug@totem9.net>
2018-02-18 22:42:41 +0000

    added some sprites

--- globals.h
+++ globals.h
@@ -4,6 +4,10 @@
 #include <cstdint>
 #include <SDL/SDL.h>
 
+extern SDL_Surface *buf;
+int  mainloop();
+void paint1();
+
 void scale2x(SDL_Surface* sf);
 void scale3x(SDL_Surface* sf);
 void scalex(SDL_Surface* sf, int sx);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "globals.h"
 using namespace std;
 
+SDL_Surface *buf = NULL;
+
+
 int main(int argc, char** argv) {
 	#if defined(__WIN32__)
 	freopen("CON", "wb", stdout);
@@ -11,28 +14,40 @@ int main(int argc, char** argv) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	
 	SDL_SetVideoMode( 640, 480, 32, SDL_HWSURFACE );
-	SDL_Surface *buf = SDL_CreateRGBSurface(SDL_SWSURFACE, 160, 144, 32, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
-	SDL_FillRect(buf, NULL, 0xff0000ff);
+	buf = SDL_CreateRGBSurface(SDL_SWSURFACE, 160, 144, 32, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
 	
-	if (1) {
-		SDL_Rect r = { 16, 16, 16, 16 };
-		SDL_FillRect(buf, &r, 0x00ff00ff);
-	}
+	mainloop();
 	
+	SDL_Quit();
+	return 0;	
+}
+
+
+int mainloop() {
+	SDL_Rect bufpos = { 24, 8, 0, 0 };
 	SDL_Event e;
 	int mainloop = 1;
+	
 	while (mainloop) {
 		while (SDL_PollEvent(&e))
 			if (e.type == SDL_QUIT)
 				mainloop = 0;
+				
+		paint1();
 		
 		SDL_FillRect( SDL_GetVideoSurface(), NULL, 0x0 );
-		SDL_Rect r = { 24, 8, 0, 0 };
-		SDL_BlitSurface( buf, NULL, SDL_GetVideoSurface(), &r );
+		SDL_BlitSurface( buf, NULL, SDL_GetVideoSurface(), &bufpos );
 		scalex( SDL_GetVideoSurface(), 3 );
 		SDL_Flip( SDL_GetVideoSurface() );
 	}
 	
-	SDL_Quit();
 	return 0;
 }
+
+
+void paint1() {
+	SDL_FillRect(buf, NULL, 0xff0000ff);
+	SDL_Rect r = { 16, 16, 16, 16 };
+	SDL_FillRect(buf, &r, 0x00ff00ff);
+}
+
